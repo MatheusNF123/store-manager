576fb65fcbff0920eb5ff94ae13c70fe6ca8ac94
Matheus <matheus_cs.nf@hotmail.com>
2022-09-10 22:20:42 -0300

    problema com avaliador

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,12 +2,19 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 
+// const connection = mysql.createPool({
+//   host: 'db',
+//   user: 'root',
+//   password: 'password',
+//   database: 'StoreManager',
+//   port: 3306,
+// });
+
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
